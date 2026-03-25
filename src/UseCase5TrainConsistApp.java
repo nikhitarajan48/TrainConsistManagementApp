3bb8bb9b01f07474f51874c174df4f9f114c08f3
nikhitarajan48 <nikhitarajan48@gmail.com>
2026-03-25 15:12:56 +0530

    feature/UC5

---
+++ src/UseCase5TrainConsistApp.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class UseCase5TrainConsistApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
+
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
+        trainFormation.add("Sleeper");
+
+        System.out.println("Train Formation: " + trainFormation);
+    }
+}
