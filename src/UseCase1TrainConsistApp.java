24eccde81ce8b294bcf2711dfc9ba1a4fabf833b
nikhitarajan48 <nikhitarajan48@gmail.com>
2026-03-25 14:49:37 +0530

    feature/UC1

---
+++ src/UseCase1TrainConsistApp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class UseCase1TrainConsistApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train consist initialized.");
+        System.out.println("Initial bogie count: " + trainConsist.size());
+    }
+}
