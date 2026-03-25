7cc91a62f907961b26e09b712eadd95c73838d12
nikhitarajan48 <nikhitarajan48@gmail.com>
2026-03-25 15:54:12 +0530

    feature/UC7

---
+++ src/UseCase7TrainConsistApp.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+}
+
+public class UseCase7TrainConsistApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+
+        for (Bogie b : bogies) {
+            System.out.println(b.name + " Capacity: " + b.capacity);
+        }
+    }
+}
