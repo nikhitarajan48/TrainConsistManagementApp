5537bcad2f69c935e838ae580801b629ba539ad6
nikhitarajan48 <nikhitarajan48@gmail.com>
2026-04-10 16:37:56 +0530

    UseCase14

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+class InvalidCapacityException extends Exception {
+    public InvalidCapacityException(String message) {
+        super(message);
+    }
+}
+
+class PassengerBogie {
+    private String type;
+    private int capacity;
+
+    public PassengerBogie(String type, int capacity) throws InvalidCapacityException {
+        if (capacity <= 0) {
+            throw new InvalidCapacityException("Capacity must be greater than zero");
+        }
+        this.type = type;
+        this.capacity = capacity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "PassengerBogie{type='" + type + "', capacity=" + capacity + "}";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<PassengerBogie> bogies = new ArrayList<>();
+
+        try {
+            bogies.add(new PassengerBogie("Sleeper", 72));
+            bogies.add(new PassengerBogie("AC Chair", 50));
+            bogies.add(new PassengerBogie("First Class", 40));
+
+            bogies.add(new PassengerBogie("Sleeper", -10));
+        } catch (InvalidCapacityException e) {
+            System.out.println(e.getMessage());
+        }
+
+        for (PassengerBogie b : bogies) {
+            System.out.println(b);
+        }
     }
 }
