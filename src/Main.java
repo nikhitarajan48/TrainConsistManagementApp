e86d30c120584d70fbe638b97aadf9a8ebda074f
nikhitarajan48 <nikhitarajan48@gmail.com>
2026-04-16 23:51:22 +0530

    Implemented UC17

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Arrays;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] bogieNames = {
+            "Sleeper",
+            "AC Chair",
+            "First Class",
+            "General",
+            "Luxury"
+        };
+
+        System.out.println("Before Sorting:");
+        System.out.println(Arrays.toString(bogieNames));
+
+        Arrays.sort(bogieNames);
+
+        System.out.println("\nAfter Sorting Alphabetically:");
+        System.out.println(Arrays.toString(bogieNames));
+
+        System.out.println("\nProgram continues...");
     }
 }
